33f62193b0ea7cd07e27e4908188080024e40191
Nik Dennler <nik.dennler@uzh.ch>
2020-03-18 19:54:58 +0100

    Changing hps

--- ECG_Heartbeat_Classification/run_mitbih.py
+++ ECG_Heartbeat_Classification/run_mitbih.py
@@ -20,7 +20,7 @@ def run(model, file_path):
     redonplat = ReduceLROnPlateau(monitor="val_acc", mode="max", patience=3, verbose=2)
     callbacks_list = [checkpoint, early, redonplat]  # early
 
-    model.fit(X, Y, epochs=1, verbose=2, callbacks=callbacks_list, validation_split=0.1)
+    model.fit(X, Y, epochs=1000, verbose=2, callbacks=callbacks_list, validation_split=0.1)
     model.load_weights(file_path)
 
 
@@ -38,9 +38,10 @@ X = np.array(df_train[list(range(187))].values)[..., np.newaxis]
 Y_test = np.array(df_test[187].values).astype(np.int8)
 X_test = np.array(df_test[list(range(187))].values)[..., np.newaxis]
 
-## LSTM
-model = get_model.rnn_lstm(nclass=5, hidden_layers=[2, 2, 2, 2])
-file_name = "mitbih_rnn"
+#
+# LSTM
+model = get_model.rnn_lstm(nclass=5)
+file_name = "mitbih_rnn_lstm"
 file_path = file_name + ".h5"
 run(model, file_path)
 # Save the entire model as a SavedModel.
@@ -53,5 +54,40 @@ f1 = f1_score(Y_test, pred_test, average="macro")
 print("Test f1 score : %s "% f1)
 acc = accuracy_score(Y_test, pred_test)
 print("Test accuracy score : %s "% acc)
+
+#
+# GRU
+model = get_model.rnn_gru(nclass=5)
+file_name = "mitbih_rnn_gru"
+file_path = file_name + ".h5"
+run(model, file_path)
+# Save the entire model as a SavedModel.
+model.save(os.path.join(model_directory, file_name))
+
+# Test and print out scores
+pred_test = model.predict(X_test)
+pred_test = np.argmax(pred_test, axis=-1)
+f1 = f1_score(Y_test, pred_test, average="macro")
+print("Test f1 score : %s "% f1)
+acc = accuracy_score(Y_test, pred_test)
+print("Test accuracy score : %s "% acc)
+
+#
+# Bidirectional GRU
+model = get_model.rnn_gru_bidir(nclass=5)
+file_name = "mitbih_rnn_gru_bidir"
+file_path = file_name + ".h5"
+run(model, file_path)
+# Save the entire model as a SavedModel.
+model.save(os.path.join(model_directory, file_name))
+
+# Test and print out scores
+pred_test = model.predict(X_test)
+pred_test = np.argmax(pred_test, axis=-1)
+f1 = f1_score(Y_test, pred_test, average="macro")
+print("Test f1 score : %s "% f1)
+acc = accuracy_score(Y_test, pred_test)
+print("Test accuracy score : %s "% acc)
+
 
 
