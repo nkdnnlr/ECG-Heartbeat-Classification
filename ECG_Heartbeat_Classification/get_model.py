925a7802dbab2989ffc75612e0625a70b44d0d06
Nik Dennler <nik.dennler@uzh.ch>
2020-03-18 19:25:24 +0100

    Adding better functions for model getter and runner

---
+++ ECG_Heartbeat_Classification/get_model.py
@@ -0,0 +1,153 @@
+import pandas as pd
+import numpy as np
+
+import tensorflow as tf
+from keras import optimizers, losses, activations, models
+from keras.callbacks import ModelCheckpoint, EarlyStopping, LearningRateScheduler, ReduceLROnPlateau
+from keras.layers import Dense, Input, Dropout, Convolution1D, MaxPool1D, GlobalMaxPool1D, GlobalAveragePooling1D, \
+    concatenate
+from sklearn.metrics import f1_score, accuracy_score
+
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Embedding, LSTM, GRU, Bidirectional
+
+
+def rnn_lstm(nclass, input_shape=(187, 1), hidden_layers=[64, 128, 64, 16], dropout=0.2):
+    """
+    RNN with Long Short Term Memory (LSTM) Units
+    :param nclass:
+    :param input_shape:
+    :param layers:
+    :param dropout:
+    :return:
+    """
+    inp = Input(shape=input_shape)
+
+    x = LSTM(hidden_layers[0], name='LSTM1', return_sequences=True, dropout=dropout)(inp)
+    x = LSTM(hidden_layers[1], name='LSTM2', dropout=dropout)(x)
+    x = Dense(hidden_layers[2], name='Dense1', activation='relu')(x)
+    x = Dense(hidden_layers[3], name='Dense2', activation='relu')(x)
+    x = Dense(nclass, name='Output', activation=activations.softmax)(x)
+
+    model = models.Model(inputs=inp, outputs=x)
+    opt = optimizers.Adam(0.001)
+
+    model.compile(optimizer=opt, loss=losses.sparse_categorical_crossentropy, metrics=['acc'])
+    model.summary()
+    return model
+
+
+def rnn_lstm_bidir(nclass, input_shape=(187, 1), hidden_layers=[64, 128, 64, 16], dropout=0.2):
+    """
+    Bidirectional RNN with Long Short Term Memory (LSTM) Units
+    :param nclass:
+    :param input_shape:
+    :param layers:
+    :param dropout:
+    :return:
+    """
+    inp = Input(shape=input_shape)
+
+    layer = LSTM(hidden_layers[0], name='LSTM1', return_sequences=True, dropout=dropout)  # (inp)
+    x = Bidirectional(layer, name='BiRNN1')(inp)
+    layer = LSTM(hidden_layers[1], name='LSTM2', dropout=dropout)  # (x)
+    x = Bidirectional(layer, name='BiRNN2')(x)
+    x = Dense(hidden_layers[2], name='Dense1', activation='relu')(x)
+    x = Dense(hidden_layers[3], name='Dense2', activation='relu')(x)
+    x = Dense(nclass, name='Output', activation=activations.softmax)(x)
+
+    model = models.Model(inputs=inp, outputs=x)
+    opt = optimizers.Adam(0.001)
+
+    model.compile(optimizer=opt, loss=losses.sparse_categorical_crossentropy, metrics=['acc'])
+    model.summary()
+    return model
+
+
+def rnn_gru(nclass, input_shape=(187, 1), hidden_layers=[64, 128, 64, 16], dropout=0.2):
+    """
+    RNN with Gated Rectified Units
+    :param nclass:
+    :param input_shape:
+    :param layers:
+    :param dropout:
+    :return:
+    """
+    inp = Input(shape=input_shape)
+
+    x = GRU(hidden_layers[0], name='GRU1', return_sequences=True, dropout=dropout)(inp)
+    x = GRU(hidden_layers[1], name='GRU2', dropout=dropout)(x)
+    x = Dense(hidden_layers[2], name='Dense1', activation='relu')(x)
+    x = Dense(hidden_layers[3], name='Dense2', activation='relu')(x)
+    x = Dense(nclass, name='Output', activation=activations.softmax)(x)
+
+    model = models.Model(inputs=inp, outputs=x)
+    opt = optimizers.Adam(0.001)
+
+    model.compile(optimizer=opt, loss=losses.sparse_categorical_crossentropy, metrics=['acc'])
+    model.summary()
+    return model
+
+
+def rnn_gru_bidir(nclass, input_shape=(187, 1), hidden_layers=[64, 128, 64, 16], dropout=0.2):
+    """
+    Bidirectional RNN with Gated Rectified Units
+    :param nclass:
+    :param input_shape:
+    :param layers:
+    :param dropout:
+    :return:
+    """
+    inp = Input(shape=input_shape)
+
+    layer = GRU(hidden_layers[0], name='GRU1', return_sequences=True, dropout=dropout)  # (inp)
+    x = Bidirectional(layer, name='BiRNN1')(inp)
+    layer = GRU(hidden_layers[1], name='GRU2', dropout=dropout)  # (x)
+    x = Bidirectional(layer, name='BiRNN2')(x)
+    x = Dense(hidden_layers[2], name='Dense1', activation='relu')(x)
+    x = Dense(hidden_layers[3], name='Dense2', activation='relu')(x)
+    x = Dense(nclass, name='Output', activation=activations.softmax)(x)
+
+    model = models.Model(inputs=inp, outputs=x)
+    opt = optimizers.Adam(0.001)
+
+    model.compile(optimizer=opt, loss=losses.sparse_categorical_crossentropy, metrics=['acc'])
+    model.summary()
+    return model
+
+
+def get_model(nclass, input_shape=(187, 1)):
+    """
+    1d ConvNet
+    :param nclass:
+    :param input_shape:
+    :return:
+    """
+    inp = Input(shape=input_shape)
+    img_1 = Convolution1D(16, kernel_size=5, activation=activations.relu, padding="valid")(inp)
+    img_1 = Convolution1D(16, kernel_size=5, activation=activations.relu, padding="valid")(img_1)
+    img_1 = MaxPool1D(pool_size=2)(img_1)
+    img_1 = Dropout(rate=0.1)(img_1)
+    img_1 = Convolution1D(32, kernel_size=3, activation=activations.relu, padding="valid")(img_1)
+    img_1 = Convolution1D(32, kernel_size=3, activation=activations.relu, padding="valid")(img_1)
+    img_1 = MaxPool1D(pool_size=2)(img_1)
+    img_1 = Dropout(rate=0.1)(img_1)
+    img_1 = Convolution1D(32, kernel_size=3, activation=activations.relu, padding="valid")(img_1)
+    img_1 = Convolution1D(32, kernel_size=3, activation=activations.relu, padding="valid")(img_1)
+    img_1 = MaxPool1D(pool_size=2)(img_1)
+    img_1 = Dropout(rate=0.1)(img_1)
+    img_1 = Convolution1D(256, kernel_size=3, activation=activations.relu, padding="valid")(img_1)
+    img_1 = Convolution1D(256, kernel_size=3, activation=activations.relu, padding="valid")(img_1)
+    img_1 = GlobalMaxPool1D()(img_1)
+    img_1 = Dropout(rate=0.2)(img_1)
+
+    dense_1 = Dense(64, activation=activations.relu, name="dense_1")(img_1)
+    dense_1 = Dense(64, activation=activations.relu, name="dense_2")(dense_1)
+    dense_1 = Dense(nclass, activation=activations.softmax, name="dense_3_mitbih")(dense_1)
+
+    model = models.Model(inputs=inp, outputs=dense_1)
+    opt = optimizers.Adam(0.001)
+
+    model.compile(optimizer=opt, loss=losses.sparse_categorical_crossentropy, metrics=['acc'])
+    model.summary()
+    return model
---
+++ ECG_Heartbeat_Classification/run_mitbih.py
@@ -0,0 +1,58 @@
+import os
+
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+
+from keras import optimizers, losses, activations, models
+from keras.callbacks import ModelCheckpoint, EarlyStopping, LearningRateScheduler, ReduceLROnPlateau
+from keras.layers import Dense, Input, Dropout, Convolution1D, MaxPool1D, GlobalMaxPool1D, GlobalAveragePooling1D, \
+    concatenate
+from sklearn.metrics import f1_score, accuracy_score
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Embedding, LSTM, GRU, Bidirectional
+
+from ECG_Heartbeat_Classification import get_model
+
+
+def run(model, file_path):
+    checkpoint = ModelCheckpoint(file_path, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
+    early = EarlyStopping(monitor="val_acc", mode="max", patience=5, verbose=1)
+    redonplat = ReduceLROnPlateau(monitor="val_acc", mode="max", patience=3, verbose=2)
+    callbacks_list = [checkpoint, early, redonplat]  # early
+
+    model.fit(X, Y, epochs=1, verbose=2, callbacks=callbacks_list, validation_split=0.1)
+    model.load_weights(file_path)
+
+
+# Make directory
+model_directory = "../models"
+if not os.path.exists(model_directory):
+    os.makedirs(model_directory)
+
+df_train = pd.read_csv("../data/heartbeat/mitbih_train.csv", header=None)
+df_train = df_train.sample(frac=1)
+df_test = pd.read_csv("../data/heartbeat/mitbih_test.csv", header=None)
+
+Y = np.array(df_train[187].values).astype(np.int8)
+X = np.array(df_train[list(range(187))].values)[..., np.newaxis]
+Y_test = np.array(df_test[187].values).astype(np.int8)
+X_test = np.array(df_test[list(range(187))].values)[..., np.newaxis]
+
+## LSTM
+model = get_model.rnn_lstm(nclass=5, hidden_layers=[2, 2, 2, 2])
+file_name = "mitbih_rnn"
+file_path = file_name + ".h5"
+run(model, file_path)
+# Save the entire model as a SavedModel.
+model.save(os.path.join(model_directory, file_name))
+
+# Test and print out scores
+pred_test = model.predict(X_test)
+pred_test = np.argmax(pred_test, axis=-1)
+f1 = f1_score(Y_test, pred_test, average="macro")
+print("Test f1 score : %s "% f1)
+acc = accuracy_score(Y_test, pred_test)
+print("Test accuracy score : %s "% acc)
+
+
